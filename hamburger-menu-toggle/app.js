46c8f6d57d2197ffe266ec950f445ad464e107f0
Kailey Waal <63416934+kaileywaal@users.noreply.github.com>
2021-08-26 15:52:28 -0600

    Update app.js

--- hamburger-menu-toggle/app.js
+++ hamburger-menu-toggle/app.js
@@ -1,5 +1,15 @@
+function toggleMenu(e) {
+    if(e.type != "resize") {
+        console.log(e.type);
+        document.querySelector('.hamburger-icon').classList.toggle('collapsible--open');
+        document.querySelector('.nav-bar').classList.toggle('nav-bar-collapsible--open');
+    }    
 
-function toggleMenu(x) {
-    x.classList.toggle('collapsible--open');
-    document.querySelector('.nav-bar').classList.toggle('collapsible--open');
+    if(window.width >= 650) {
+        console.log("if")
+        document.querySelector('.hamburger-icon').classList.remove('collapsible--open');
+        document.querySelector('.nav-bar').classList.remove('nav-bar-collapsible--open');
+    } 
 }
+
+window.addEventListener("resize", toggleMenu);
